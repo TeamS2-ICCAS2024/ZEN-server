30fa6d001a987b2c8479999ec6c3ac9bf348ec6d
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-07-18 18:50:51 +0900

    fix : conflict 해결

--- ZenServer/src/main/java/com/zen/ZenServer/api/user/domain/User.java
+++ ZenServer/src/main/java/com/zen/ZenServer/api/user/domain/User.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "_user")
+
 public class User implements UserDetails {
 
     @Id
@@ -28,6 +29,7 @@ public class User implements UserDetails {
   
     private Long leaf;
 
+    @Setter
     private Long background_id;
 
 
